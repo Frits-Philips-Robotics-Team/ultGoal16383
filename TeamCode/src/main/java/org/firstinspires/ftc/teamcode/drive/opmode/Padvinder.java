8ce397fda1cb99e554fd830ed7266dde486256ef
Frits-Philips-Robotics <45404718+Frits-Philips-Robotics@users.noreply.github.com>
2020-09-04 17:31:42 +0200

    Tried making trajectory

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Padvinder.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryConstraints;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+@Autonomous(group = "drive")
+public class Padvinder extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        Pose2d startPose = new Pose2d(-64, -62, 0);
+
+        drive.setPoseEstimate(startPose);
+
+        Trajectory traj = drive.trajectoryBuilder(new Pose2d(-64, -62, 0))
+                .splineTo(new Vector2d(0, -48), 0)
+                .splineTo(new Vector2d(0, 0), Math.toRadians(90))
+                .build();
+
+        waitForStart();
+
+        drive.followTrajectory(traj);
+    }
+}
